a3567ce9095a41c468343f706583fc22e719c0cc
Mayurwagh98 <mswagh1998@gmail.com>
2023-05-26 21:59:04 +0530

    Added: loder icon in button

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-let pink_btn = document.querySelector(".pink").addEventListener("click", function(){
 
-    document.querySelector(".umbrella_image").src="./assessts/Pink umbrella.png"
+let pink_btn = document.querySelector(".pink").addEventListener("click", function(){
+  
+    document.querySelector(".umbrella_image").src = "./assessts/Pink umbrella.png"
 
     document.querySelector(".custom-file-upload").style.backgroundColor = "#d8348b"
 
@@ -8,8 +9,8 @@ let pink_btn = document.querySelector(".pink").addEventListener("click", functio
 })
 
 let blue_btn = document.querySelector(".blue").addEventListener("click", function(){
-
-    document.querySelector(".umbrella_image").src="./assessts/Blue umbrella.png"
+ 
+    document.querySelector(".umbrella_image").src = "./assessts/Blue umbrella.png"
 
     document.querySelector(".custom-file-upload").style.backgroundColor = "#2eb3e5"
 
@@ -17,9 +18,9 @@ let blue_btn = document.querySelector(".blue").addEventListener("click", functio
 })
 
 let yellow_btn = document.querySelector(".yellow").addEventListener("click", function(){
-
-    document.querySelector(".umbrella_image").src="./assessts/Yello umbrella.png"
-
+  
+    document.querySelector(".umbrella_image").src = "./assessts/Yello umbrella.png"
+    
     document.querySelector(".custom-file-upload").style.backgroundColor = "#fed247"
 
     document.body.style.backgroundColor = "#FFFFD0"
@@ -28,29 +29,33 @@ let yellow_btn = document.querySelector(".yellow").addEventListener("click", fun
 
 let uploadLogo = document.getElementById('file-upload').addEventListener('change', function (e) {
 
-        
-            var file = e.target.files[0]; // Get the selected file
+          var file = e.target.files[0]; // Get the selected file
            
-            var reader = new FileReader();
-          
-            reader.onload = function (event) {
-              var image = new Image();
-              image.src = event.target.result; // Set the source of the image to the uploaded file
-              image.setAttribute("class","uploaded_image")
-              image.onload = function () {
-                document.getElementById('logo_div').innerHTML = ''; // Clear previous image
-                document.getElementById('logo_div').appendChild(image); // Append the image to the logo_div element
-              };
-            };
-            
-            reader.readAsDataURL(file); // Read the file as a data URL   
-            
-            let {name} = file // destructuring file name 
-
-            let loader_icon_img = document.querySelector(".upload_icon_img")
-            loader_icon_img.src = "./assessts/loader_icon.svg"
+          var reader = new FileReader();
         
-            document.querySelector(".display_file_name").textContent = name// displaying file name on button
+          reader.onload = function (event) {
+            var image = new Image();
+            image.src = event.target.result; // Set the source of the image to the uploaded file
+            image.setAttribute("class","uploaded_image")
+            image.onload = function () {
+              document.getElementById('logo_div').innerHTML = ''; // Clear previous image
+              document.getElementById('logo_div').appendChild(image); // Append the image to the logo_div element
+            };
+          };
+          
+          setTimeout(() =>{
+            reader.readAsDataURL(file); // Read the file as a data URL   
+          },2000)
+          
+          let {name} = file // destructuring file name 
+
+          let loader_icon_img = document.querySelector(".upload_icon_img")
+          loader_icon_img.src = "./assessts/loader_icon.svg"
+          loader_icon_img.setAttribute("class","loader_icon_img")
+      
+          document.querySelector(".display_file_name").textContent = name// displaying file name on button
+          
+        
             
   });
 
