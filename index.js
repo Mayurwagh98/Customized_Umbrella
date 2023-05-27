fb6aa0f47a02e3606f1cda6142d6f628f3c683c2
Mayurwagh98 <mswagh1998@gmail.com>
2023-05-27 08:22:22 +0530

    Added: remove functionality

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let umbrellaFun = (item,bgc,bodycolor) =>{
+let umbrellaFun = (item, bgc, bodycolor) =>{
   document.querySelector(".umbrella_image").src = `./assessts/${item}`
   document.querySelector(".custom-file-upload").style.backgroundColor = bgc
   document.body.style.backgroundColor = bodycolor
@@ -18,12 +18,14 @@ let yellow_btn = document.querySelector(".yellow").addEventListener("click", fun
 
 
 let uploadLogo = document.getElementById('file-upload').addEventListener('change', function (e) {
-
           var file = e.target.files[0]; // Get the selected file
            
           var reader = new FileReader();
         
+          let {name} = file // destructuring file name 
+          
           reader.onload = function (event) {
+            
             var image = new Image();
             image.src = event.target.result; // Set the source of the image to the uploaded file
             image.setAttribute("class","uploaded_image")
@@ -31,21 +33,31 @@ let uploadLogo = document.getElementById('file-upload').addEventListener('change
               document.getElementById('logo_div').innerHTML = ''; // Clear previous image
               document.getElementById('logo_div').appendChild(image); // Append the image to the logo_div element
             };
+            document.querySelector(".display_file_name").textContent = name// displaying file name on button
           };
           
           setTimeout(() =>{
             reader.readAsDataURL(file); // Read the file as a data URL   
           },2000)
           
-          let {name} = file // destructuring file name 
-
           let loader_icon_img = document.querySelector(".upload_icon_img")
           loader_icon_img.src = "./assessts/loader_icon.svg"
           loader_icon_img.setAttribute("class","loader_icon_img")
-      
-          document.querySelector(".display_file_name").textContent = name// displaying file name on button
           
+          let remove_span = document.createElement("span")
+          remove_span.textContent = "X"
+          remove_span.setAttribute("class", "remove_btn")
+        
+          document.querySelector(".custom-file-upload").append(remove_span)
+          
+            document.querySelector(".remove_btn").addEventListener("click", function(){
+              let fileelement =  document.getElementById('logo_div')
+              fileelement.remove()
+              document.querySelector(".display_file_name").textContent = "upload logo"
+              location.reload()
+          })
   });
+
 
   
   
